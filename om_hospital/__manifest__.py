b8b8e9d05c40258a754d21f6fadeb8046a931017
Just S1 <hinchanritheavuth20@kit.edu.kh>
2022-12-07 13:03:43 +0700

    first commit

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name' : 'Hospital Management',
+    'version' : '1.0',
+    'summary': 'Hospital Management Software',
+    'sequence': 10,
+    'description': """Hospital Management Software""",
+    'category': 'Productivity',
+    'website': '',
+    'license': 'LGPL-3',
+    'depends' : [],
+    'data': [
+        'views/patient.xml'
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ om_hospital/models/patient.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+
+class HospitalPatient(models.Model):
+    _name = "hospital.patient"
+    _description = "Hospital Patient"
+
+    name = fields.Char(string='Name', required=True, translate=True)
+    age = fields.Integer(string='Age')
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other'),
+    ], required=True, default='male')
+    note = fields.Text(string='Description')
